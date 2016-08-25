ec5029b793355ee2d4cc66341e92fbaf38744fda
thejsj <jorge.silva@thejsj.com>
2016-08-25 15:54:38 -0700

    Follow google style guide

--- .helpers.d/big-poppa.sh
+++ .helpers.d/big-poppa.sh
@@ -1,38 +1,38 @@
 ## Big Poppa Utility Functions
 
 # Get a Big Poppa org by its id
-function bp_org_get_by_id
+function bp::org_get_by_id
 {
 	ssh delta-app-services curl 0.0.0.0:7788/organization/$1 | python -m json.tool
 }
 
 # Get a Big Poppa user by its id
-function bp_user_get_by_id
+function bp::user_get_by_id
 {
 	ssh delta-app-services curl 0.0.0.0:7788/user/$1 | python -m json.tool
 }
 
 # Get a Big Poppa organization by its Github id
-function bp_org_get_by_github_id
+function bp::org_get_by_github_id
 {
 	ssh delta-app-services curl 0.0.0.0:7788/organization/?githubId=$1 | python -m json.tool
 }
 
 # Get a Big Poppa user by its Github id
-function bp_user_get_by_github_id
+function bp::user_get_by_github_id
 {
 	ssh delta-app-services curl 0.0.0.0:7788/user/?githubId=$1 | python -m json.tool
 }
 
 # Get a Big Poppa organization by its Github login
-function bp_org_get_by_name
+function bp::org_get_by_name
 {
 	lower_name=$(echo $1 | awk '{print tolower($0)}')
 	ssh delta-app-services curl 0.0.0.0:7788/organization/?lowerName=$lower_name | python -m json.tool
 }
 
 # Get a Big Poppa user by its Github login
-function bp_user_get_by_name
+function bp::user_get_by_name
 {
 	lower_name=$(echo $1 | awk '{print tolower($0)}')
 	# BP has no knowledge of Github login, so we have to query this from GH
--- .helpers.d/github.sh
+++ .helpers.d/github.sh
@@ -1,19 +1,19 @@
 ## Github Utility Functions
 
 # Get a github organization/user by its ID
-function github_get_by_id
+function github::get_by_id
 {
 	curl -s https://api.github.com/user/$1 | python -m json.tool
 }
 
 # Get a github user by its login/username
-function github_get_by_username
+function github::get_by_username
 {
 	curl -s https://api.github.com/users/$1 | python -m json.tool
 }
 
 # Get a github organization by its login/username
-function github_get_by_orgname
+function github::get_by_orgname
 {
 	curl -s https://api.github.com/orgs/$1 | python -m json.tool
 }
