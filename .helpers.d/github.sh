e06063bd46ea585b25437bb7b1f76631c98e2b88
thejsj <jorge.silva@thejsj.com>
2016-08-25 15:49:07 -0700

    Change to _ case

--- .helpers.d/github.sh
+++ .helpers.d/github.sh
@@ -1,19 +1,19 @@
 ## Github Utility Functions
 
 # Get a github organization/user by its ID
-function github-get-by-id
+function github_get_by_id
 {
 	curl -s https://api.github.com/user/$1 | python -m json.tool
 }
 
 # Get a github user by its login/username
-function github-get-by-username
+function github_get_by_username
 {
 	curl -s https://api.github.com/users/$1 | python -m json.tool
 }
 
 # Get a github organization by its login/username
-function github-get-by-orgname
+function github_get_by_orgname
 {
 	curl -s https://api.github.com/orgs/$1 | python -m json.tool
 }
